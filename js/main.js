ee01957b872dede0f62ed2017a13d8267a2e5fd1
isaccruz78 <157855886+isaccruz78@users.noreply.github.com>
2024-02-26 13:07:03 -0300

    landing page

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* Funções para abrir e fechar o menu mobile de acordo com o botão clicado */
+function AbrirMenuMobile(){
+    document.getElementById("menu_mobile").style.width = '100%';
+}
+
+function FecharMenuMobile(){
+    document.getElementById("menu_mobile").style.width = '0';
+}
